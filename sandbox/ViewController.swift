b5a750e679be3c1590df6c41d3a3f3cbdefe7d7e
Kousha moaveninejad <KoushaNejad@koushas-macbook-pro.local>
2016-09-20 20:32:21 -0500

    just a test

--- sandbox/ViewController.swift
+++ sandbox/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //this comment has been added by Kousha
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
